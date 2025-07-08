e3bedbc9532e3dd8c72fb5a259366c62726ef66a
Felix Wiegand <fxwiegand@wgdnet.de>
2025-07-08 13:07:09 +0200

    Create setup.sh

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+echo "Setting up Datavzrd tutorial environment..."
+
+# Clean up workspace
+find . -mindepth 1 -delete
+
+# Create data directory and download example files
+mkdir -p data
+curl -L https://raw.githubusercontent.com/datavzrd/datavzrd/main/.examples/data/movies.csv -o data/movies.csv
+curl -L https://raw.githubusercontent.com/datavzrd/datavzrd/main/.examples/data/oscars.csv -o data/oscars.csv
+
+# Install micromamba
+bash <(curl -L micro.mamba.pm/install.sh)
+
+# Activate micromamba and install datavzrd
+source ~/.bashrc
+micromamba create -n datavzrd -c conda-forge datavzrd --yes
+echo "micromamba activate datavzrd" >> ~/.bashrc
+
+echo "alias show-report-url='echo https://0.0.0.0:8000/example-report'" >> ~/.bashrc
+
+# Activate for current shell
+source ~/.bashrc
+micromamba activate datavzrd
+
+echo "Setup complete!"
